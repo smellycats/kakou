3c2e81c13e1ecea6b8ba47e73beed1d756764f99
SmellyCat <smellycat2014@foxmail.com>
2015-05-20 16:18:53 +0800

    Fix user_add name error

--- application/views/user/user_edit.php
+++ application/views/user/user_edit.php
@@ -19,15 +19,15 @@
 		<div id="pollxxPanel" class="pageFormContent" layoutH="58">
 			<div class="unit">
 				<label>用户名：</label>
-				<input id="user_add_username" name="user_add_username" readonly="true" type="text" maxlength="16"/>
+				<input id="user_edit_username" name="user_edit_username" readonly="true" type="text" maxlength="16"/>
 			</div>
 			<div class="unit">
 				<label>真实名：</label>
-				<input id="user_add_realname" name="user_add_realname" type="text" maxlength="16" />
+				<input id="user_edit_realname" name="user_edit_realname" type="text" maxlength="16" />
 			</div>
 			<div class="unit">
 				<label>角色：</label>
-				<select id="user_add_role_id" name="user_add_role_id" class="required combox">
+				<select id="user_edit_role_id" name="user_edit_role_id" class="required combox">
 					<?php foreach ($roles as $row): ?>
 					<option value="<?php echo $row['id']; ?>"><?php echo $row['name']; ?></option>
 					<?php endforeach; ?>
@@ -35,32 +35,32 @@
 			</div>
 			<div class="unit">
 				<label>身份证号码：</label>
-				<input id="user_add_identity" name="user_add_identity" type="text" maxlength="32" />
+				<input id="user_edit_identity" name="user_edit_identity" type="text" maxlength="32" />
 			</div>
 			<div class="unit">
 				<label>联系电话：</label>
-				<input id="user_add_phone" name="user_add_phone" type="text" maxlength="16" />
+				<input id="user_edit_phone" name="user_edit_phone" type="text" maxlength="16" />
 			</div>
 			<div class="unit">
 				<label>单位：</label>
-				<input id="user_add_department" name="user_add_department" type="text" maxlength="40" />
+				<input id="user_edit_department" name="user_edit_department" type="text" maxlength="40" />
 			</div>
 			<div class="unit">
 				<label>用户状态：</label>
-				<input id="user_add_banned" name="user_add_banned" type="checkbox" value="1" />
+				<input id="user_edit_banned" name="user_edit_banned" type="checkbox" value="1" />
 				<span style="color:blue">锁定 (锁定帐户,使其无法登录系统)</span>
 			</div>
 			<div class="unit">
 				<label>登录模式：</label>
-				<input id="at0" name="user_add_access_type" type="radio" value="0" checked="checked"/>帐号密码<input id="at1" type="radio" name="user_add_access_type" value="1"/>数字证书<input id="at2" type="radio" name="user_add_access_type" value="2"/>混合模式
+				<input id="at0" name="user_edit_access_type" type="radio" value="0" checked="checked"/>帐号密码<input id="at1" type="radio" name="user_add_access_type" value="1"/>数字证书<input id="at2" type="radio" name="user_add_access_type" value="2"/>混合模式
 			</div>
 			<div class="unit">
 				<label>限制IP访问：</label>
-				<textarea id="user_add_limit_login_address" name="user_add_limit_login_address" style="height:60px;" ></textarea>
+				<textarea id="user_edit_limit_login_address" name="user_edit_limit_login_address" style="height:60px;" ></textarea>
 			</div>
 			<div class="unit">
 				<label>备注：</label>
-				<textarea id="user_add_memo" name="user_add_memo" style="height:60px;" ></textarea>
+				<textarea id="user_edit_memo" name="user_edit_memo" style="height:60px;" ></textarea>
 			</div>
 		</div>
 		<div class="formBar">
