689ab762c759c9923d135dd9f2091bf097a9fcf2
SmellyCat <smellycat2014@foxmail.com>
2015-05-23 14:59:47 +0800

    Fix bug views\logo\lgquery_view

--- application/views/logo/lgquery_view.php
+++ application/views/logo/lgquery_view.php
@@ -7,6 +7,7 @@
 		$("#lgquery_view_hpys").val(<?php echo '"'. $hpys .'"'?>);
 		$("#lgquery_view_ppdm").val(<?php echo '"'. $ppdm .'"'?>);
 		$("#lgquery_view_ppdm2").val(<?php echo '"'. $ppdm2 .'"'?>);
+		$("#lgquery_view_cllx").val(<?php echo '"'. $cllx .'"'?>);
 		$("#lgquery_view_st").val(<?php echo '"'. $st .'"'?>);
 		$("#lgquery_view_et").val(<?php echo '"'. $et .'"'?>);
 		$("#lgquery_view_number").val(<?php echo '"'. $number .'"'?>);
@@ -25,6 +26,7 @@
 	<input type="hidden" name="hpys" value="<?php echo $hpys; ?>" />
 	<input type="hidden" name="ppdm" value="<?php echo $ppdm; ?>" />
 	<input type="hidden" name="ppdm2" value="<?php echo $ppdm2; ?>" />
+	<input type="hidden" name="cllx" value="<?php echo $cllx; ?>" />
 	<input type="hidden" name="st" value="<?php echo $st; ?>" />
 	<input type="hidden" name="et" value="<?php echo $et; ?>" />
 	<input type="hidden" name="number" value="<?php echo $number; ?>" />
@@ -65,17 +67,15 @@
 					</select>
 				</td>
 				<td>
-					<label>车辆品牌：</label>
-					<select class="combox" id="lgquery_view_ppdm" name="ppdm" ref="lgquery_view_ppdm2" refUrl="<?php echo base_url('index.php/logo/get_ppdm2?ppdm=');?>{value}">
+					<label>车辆类型：</label>
+					<select class="combox" id="lgquery_view_cllx" name="cllx" >
 						<option value="all">所有</option>
-						<?php foreach ($sel_ppdm as $row): ?>
-						<option value="<?php echo $row['code']; ?>"><?php echo $row['name']; ?></option>
+						<?php foreach ($sel_cllx as $row): ?>
+						<option value="<?php echo $row['code']; ?>"><?php echo $row['code'] . $row['name']; ?></option>
 						<?php endforeach; ?>
 					</select>
-					<select class="combox" id="lgquery_view_ppdm2" name="ppdm2">
-						<option value="all">请选择主品牌</option>
-					</select>
 				</td>
+				<td></td>
 			<tr>
 			<tr>
 				<td>
@@ -87,15 +87,25 @@
 					<input type="text" id="lgquery_view_et" name="et" class="date" dateFmt="yyyy-MM-dd HH:mm:ss" readonly="true"/>
 				</td>
 				<td>
+					<label>车辆品牌：</label>
+					<select class="combox" id="lgquery_view_ppdm" name="ppdm" ref="lgquery_view_ppdm2" refUrl="<?php echo base_url('index.php/logo/get_ppdm2?ppdm=');?>{value}">
+						<option value="all">所有</option>
+						<?php foreach ($sel_ppdm as $row): ?>
+						<option value="<?php echo $row['code']; ?>"><?php echo $row['name']; ?></option>
+						<?php endforeach; ?>
+					</select>
+					<select class="combox" id="lgquery_view_ppdm2" name="ppdm2">
+						<option value="all">请选择主品牌</option>
+					</select>
+				</td>
+				<td>
 					<label>车牌号码：</label>
 					<select class="combox" id="lgquery_view_number" name="number">
 						<?php foreach ($sel_number as $row): ?>
 						<option value="<?php echo $row; ?>"><?php echo $row; ?></option>
 						<?php endforeach; ?>
 					</select>
-				</td>
-				<td>
-					<input type="text" id="lgquery_view_carnum" name="carnum" value="" />
+					<input type="text" id="lgquery_view_carnum" name="carnum" size="10" />
 				</td>
 				<td>
 					<div class="buttonActive"><div class="buttonContent"><button type="submit">检索</button></div></div>
@@ -111,20 +121,17 @@
 	<div class="panelBar">
 		<ul class="toolBar">
 			<li>
-				<a class="add" href="<?php echo base_url(); ?>index.php/user/user_add_view" 
-					target="dialog" minable="true" rel="user_index_add" max="false" drawable="false" resizable="false" 
-					maxable="true" mask="true" width="600" height="500" title="用户添加"><span>添加</span></a>
-			</li>
-			<li>
-				<a class="edit" href="<?php echo base_url(); ?>index.php/user/user_edit_view?id={user_id}" 
-					target="dialog" minable="true" rel="user_index_edit" max="false" drawable="false" resizable="false" 
-					maxable="true" mask="true" width="600" height="500" title="用户编辑"><span>编辑</span></a>
-			</li>
-			<li>
-				<a class="delete" href="<?php echo base_url(); ?>index.php/user/user_del?id={user_id}" 
-					target="ajaxTodo" title="确定要删除吗?" ><span>删除</span></a>
+				<a class="edit" href="<?php echo base_url(); ?>index.php/logo/lgquery_detail?id={lgquery_id}" 
+					target="dialog" minable="true" rel="lgquery_index_detail" max="false" drawable="false" resizable="false" 
+					maxable="true" mask="true" width="600" height="500" title="查看详细信息"><span>查看</span></a>
 			</li>
 			<li class="line">line</li>
+			<li>
+				<a class="icon" href="<?php echo base_url(); ?>index.php/export/export_excel" target="dwzExport" targetType="navTab" title="实要导出这些记录吗?"><span>导出EXCEL</span></a>
+			</li>
+			<li>
+				<a class="icon" href="<?php echo base_url(); ?>index.php/export/export_img" target="dwzExport" targetType="navTab" title="实要导出这些图片吗?"><span>导出图片</span></a>
+			</li>
 		</ul>
 	</div>
 	<table class="table" width="100%" layoutH="145">
@@ -140,8 +147,10 @@
 				 	<?php if($sort == 'ppdm'){echo 'class="'.$order.'"';}?>>车辆主品牌</th>
 				<th width="100" orderField="clpp"
 					<?php if($sort == 'clpp'){echo 'class="'.$order.'"';}?>>车辆子品牌</th>
-				<th width="50">车辆类型</th>
-				<th width="30">车身颜色</th>
+				<th width="50" orderField="cllx"
+					<?php if($sort == 'cllx'){echo 'class="'.$order.'"';}?>>车辆类型</th>
+				<th width="30" orderField="csys"
+					<?php if($sort == 'csys'){echo 'class="'.$order.'"';}?>>车身颜色</th>
 			</tr>
 		</thead>
 		<tbody>
