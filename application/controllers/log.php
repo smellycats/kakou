2c59c8e1673a06548486f8ad159ab34e8b076a82
SmellyCat <smellycat2014@foxmail.com>
2015-05-26 01:26:17 +0800

    Remove useless method from log

--- application/controllers/log.php
+++ application/controllers/log.php
@@ -29,47 +29,6 @@ class Log extends CI_Controller
 		$this->load->config('kakou');
 		//$this->output->enable_profiler(TRUE);
 	}
-	
-	//操作日志列表
-	function oplog()
-	{
-		$data['page'] = $this->input->post('page') ? intval($this->input->post('page')) : 1;
-		$data['rows'] = $this->input->post('rows') ? intval($this->input->post('rows')) : 20;
-		$data['sort'] = $this->input->post('sort') ? $this->input->post('sort') : 'czsj';
-	    $data['order'] = $this->input->post('order') ? $this->input->post('order') : 'desc';
-		$data['offset'] = ($data['page'] - 1) * $data['rows'];
-		
-		$data['st'] = $this->input->post('st') ? $this->input->post('st') : mdate("%Y-%m-%d %H:%i:%s",strtotime("-24 hours"));;
-		$data['et'] = $this->input->post('et') ? $this->input->post('et') : mdate("%Y-%m-%d %H:%i:%s");
-	    $data['uname'] = $this->input->post('uname') ? $this->input->post('uname') : '';
-
-		$data['result'] = $this->Muserlog->get_userlogs($data['offset'],$data['rows'],$data['sort'],$data['order'],$data)->result_array();
-		$data['total'] = $this->Muserlog->get_userlogs(0,0,$data['sort'],$data['order'],$data)->row()->sum;
-
-		$data['title'] = '操作日志列表';
-
-		$this->load->view('userlog/oplog_view', $data);
-	}
-	
-	//用户日志列表
-	function userlog()
-	{
-		$data['page'] = $this->input->post('page') ? intval($this->input->post('page')) : 1;
-		$data['rows'] = $this->input->post('rows') ? intval($this->input->post('rows')) : 20;
-		$data['sort'] = $this->input->post('sort') ? strval($this->input->post('sort')) : 'access_count';
-	    $data['order'] = $this->input->post('order') ? strval($this->input->post('order')) : 'desc';
-		$data['offset'] = ($data['page'] - 1) * $data['rows'];
-		
-	    $data['username'] = $this->input->post('username') ? $this->input->post('username') : '';
-	    $data['department'] = '';
-
-		$data['result'] = $this->Muser->get_users($data['offset'],$data['rows'],$data['sort'],$data['order'],$data)->result_array();
-		$data['total'] = $this->Muser->get_users(0,0,$data['sort'],$data['order'],$data)->row()->sum;
-
-		$data['title'] = '用户列表';
-
-		$this->load->view('userlog/userlog_view', $data);
-	}
 
 
 }
