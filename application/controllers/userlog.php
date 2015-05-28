3db8d380ebb5670b35cafe5a645575d9156275f5
SmellyCat <smellycat2014@foxmail.com>
2015-05-29 00:06:16 +0800

    Update improve userlog

--- application/controllers/userlog.php
+++ application/controllers/userlog.php
@@ -19,7 +19,6 @@ class Userlog extends Admin_Controller
 		$this->load->helper('url');
 		$this->load->helper('date');
 
-		$this->load->model('Muser');
 		$this->load->model('Muserlog');
 				
 		$this->load->config('kakou');
@@ -29,17 +28,16 @@ class Userlog extends Admin_Controller
 	//用户日志列表
 	function view()
 	{
-		$data['page'] = $this->input->post('page') ? intval($this->input->post('page')) : 1;
-		$data['rows'] = $this->input->post('rows') ? intval($this->input->post('rows')) : 20;
-		$data['sort'] = $this->input->post('sort') ? strval($this->input->post('sort')) : 'access_count';
-	    $data['order'] = $this->input->post('order') ? strval($this->input->post('order')) : 'desc';
+		$data['page']   = $this->input->post('page') ? intval($this->input->post('page')) : 1;
+		$data['rows']   = $this->input->post('rows') ? intval($this->input->post('rows')) : 20;
+		$data['sort']   = $this->input->post('sort') ? strval($this->input->post('sort')) : 'access_count';
+	    $data['order']  = $this->input->post('order') ? strval($this->input->post('order')) : 'desc';
 		$data['offset'] = ($data['page'] - 1) * $data['rows'];
 		
 	    $data['username'] = $this->input->post('username') ? $this->input->post('username') : '';
-	    $data['department'] = '';
 
-		$data['result'] = $this->Muser->getUsers($data['offset'],$data['rows'],$data['sort'],$data['order'],$data)->result_array();
-		$data['total'] = $this->Muser->getUsers(0,0,$data['sort'],$data['order'],$data)->row()->sum;
+		$data['result'] = $this->Muserlog->getUsers($data['offset'], $data['rows'], $data['sort'], $data['order'], $data)->result_array();
+		$data['total']  = $this->Muserlog->getUsers(0, 0, $data['sort'], $data['order'], $data)->row()->sum;
 
 		$data['title'] = '用户列表';
 
--- application/models/muserlog.php
+++ application/models/muserlog.php
@@ -13,15 +13,64 @@ class Muserlog extends CI_Model
 	{
 		parent::__construct();
 	}
-	
-	//获取用户信息
-	function get_users()
-	{
-		return $this->db->get('users');
+
+    /**
+     * 根据条件查询用户信息
+     * 
+     * @param int $offset 偏移量
+     * @param int $limit 行数
+     * @param string $sort 排序字段
+     * @param string $order 排序方向
+     * @param array $data 查询数组
+     * @return object
+     */
+	public function getUsers($offset = 0, $limit = 0, $sort = 'u.id', $order = 'asc', $data)
+	{	
+		if ($data['username'] != ''){
+			$this->db->like('username', $data['username']); 
+		}
+		
+		if ($offset == 0 and $limit == 0){
+			$this->db->select('count(*) as sum');
+		} else {
+			$this->db->select('u.id');
+			$this->db->select('r.name as rolename');
+			$this->db->select('role_id');
+			$this->db->select('username');
+			$this->db->select('realname');
+			$this->db->select('department');
+			$this->db->select('created');
+			$this->db->select('banned');
+			$this->db->select('last_ip');
+			$this->db->select('last_login');
+			$this->db->select('identity');
+			$this->db->select('phone');
+			$this->db->select('access_type');
+			$this->db->select('access_count');
+			$this->db->select('limit_login_address');
+			$this->db->select('memo');
+
+			$this->db->limit($limit, $offset);
+			$this->db->order_by($sort, $order);
+		}
+		$this->db->where('disabled', 0);
+		$this->db->where('r.del', 0);
+		$this->db->join('roles as r', 'u.role_id=r.id');
+		
+		return $this->db->get('users as u');
 	}
 	
-	//获取用户操作日志
-	function get_userlogs($offset=0, $limit=0, $sort='czsj', $order='desc', $data)
+    /**
+     * 获取用户操作日志
+     * 
+     * @param int $offset 偏移量
+     * @param int $limit 行数
+     * @param string $sort 排序字段
+     * @param string $order 排序方向
+     * @param array $data 查询数组
+     * @return object
+     */
+	function getUserlogs($offset = 0, $limit = 0, $sort = 'czsj', $order = 'desc', $data)
 	{
 		if ($data['st'] != '') {
 			$this->db->where('czsj >=',$data['st']);
@@ -39,23 +88,33 @@ class Muserlog extends CI_Model
 		} else {
 			$this->db->select('*');
 			$this->db->limit($limit, $offset);
-			$this->db->order_by($sort,$order);
+			$this->db->order_by($sort, $order);
 		}
 		
 		return $this->db->get('userlog');;
 	}
 	
-	//添加用户日志
-	function add_userlog($data)
+    /**
+     * 添加用户日志
+     * 
+     * @param array $data 数组
+     * @return object
+     */
+	function addUserlog($data)
 	{
 		return $this->db->insert('userlog', $data); 
 	}
 
-	//编辑用户日志
-	function edit_userlog($id, $data)
+    /**
+     * 编辑用户日志
+     * 
+     * @param int $id id编号
+     * @param array $data 数组
+     * @return object
+     */
+	function setUserlog($id, $data)
 	{
-		$this->db->where('id',$id);
-		return $this->db->update('userlog',$data);
+		return $this->db->where('id', $id)->update('userlog', $data);
 	}
 	
 }
