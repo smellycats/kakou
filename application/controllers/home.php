bed037d76ea0e81eb5e0e61caa5f39620753b0d4
SmellyCat <smellycat2014@foxmail.com>
2015-05-29 05:04:40 +0800

    Update controllers\home improve login

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -15,14 +15,17 @@ class Home extends CI_Controller
 	{
 		parent::__construct();
 		
-		$this->load->library('Lib_Kakou');			
-		
 		$this->load->helper('url');
 		$this->load->helper('form');
 		$this->load->helper('kakou');
 		$this->load->helper('date');
+
+		$this->load->library('Lib_Kakou');			
+		$this->load->library('form_validation');
 		
 		$this->load->model('Mhome');
+
+		$this->load->config('kakou');
 		
 		//$this->output->enable_profiler(TRUE);
 	}
@@ -36,69 +39,68 @@ class Home extends CI_Controller
 	{
 		$this->login();
 	}
-
-	public function login()
-	{	
-		$this->load->view('admin/login');
-	}
 	
-	public function login_ok()
+	public function login()
 	{
-		$data['page_title'] = '卡口系统';
+		$data['message'] = '';
+		if  (!$this->input->post(NULL)) {
+			$this->load->view('login', $data);
+			return;
+		}
 		
 		$val = $this->form_validation;
-				
 		$val->set_rules('username', '用户名', 'trim|required|xss_clean|');
 		$val->set_rules('password', '密码', 'trim|required|xss_clean');
 		$val->set_rules('remember', '记住登录状态', 'integer');
-		    	
 		$val->set_message('required', '%s不能为空.');
-	
 		$val->set_error_delimiters('<span>', '</span>');
-			    
-		$data['message'] = '';
 		
 		$query = $this->Mhome->getUserByName($this->input->post('username', True));
-
+		// 验证出错
 		if ($val->run() == False) {
-			$this->load->view('admin/login', $data);
-			exit;
+			$this->load->view('login', $data);
+			return;
 		} 
+		// 用户不存在或相关角色不存在
 		if ($query->num_rows() == 0) {
 			$data['message'] = '用户名或密码错误';
-			$this->load->view('admin/login', $data);
-			exit;
+			$this->load->view('login', $data);
+			return;
 		}
-		if ($this->input->post('password') != sha1($query->row->password)) {
+		// 密码不正确
+		if (sha1($this->input->post('password')) != $query->row()->password) {
 			$data['message'] = '用户名或密码错误';
-			$this->load->view('admin/login', $data);
-			exit;
+			$this->load->view('login', $data);
+			return;
 		}
-		// 检查用户IP
+		var_dump($this->session->userdata('ip_address'));
+		// 检查全局IP限制
 		if ($this->_checkIpAccess($this->session->userdata('ip_address')) == False) {
 			$data['message'] = '限制IP登陆';
-			$this->load->view('admin/login', $data);
-			exit;
+			$this->load->view('login', $data);
+			return;
 		}
+		// 角色被冻结
 		if ($query->row()->r_banned == 1) {
 			$data['message'] = '用户所属角色已被冻结';	
-			$this->load->view('admin/login', $data);
-			exit;
+			$this->load->view('login', $data);
+			return;
 		}
+		// 用户被冻结
 		if($query->row()->u_banned == 1){
 			$data['message'] = '用户已被冻结';
-			$this->load->view('admin/login', $data);
-			exit;
+			$this->load->view('login', $data);
+			return;
 		}
 		// 保存session
 		$this->_setSession($query->row());
 		// 修改用户登录信息
 		$da['last_ip']    = $this->session->userdata('ip_address');
 		$da['last_login'] = mdate("%Y-%m-%d %H:%i:%s");
-		$this->Mhome->loginPlus($query->row()->id, $da);
+		$this->Mhome->loginPlus($query->row()->user_id, $da);
 		// 添加登录日志
 		$da2['ip']        = $this->session->userdata('ip_address');
-		$da2['user_id']   = $query->row()->id;
+		$da2['user_id']   = $query->row()->user_id;
 		$da2['user_name'] = $query->row()->user_name;
 		$this->Mhome->addAccessLog($da2);
 				
