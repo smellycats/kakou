002a7019ce1ab2e6584195b0546786f155c7ab03
SmellyCat <smellycat2014@foxmail.com>
2015-05-28 19:54:59 +0800

    Update logo

--- application/controllers/logo.php
+++ application/controllers/logo.php
@@ -10,7 +10,7 @@
  *
  */
 
-class Logo extends CI_Controller
+class Logo extends Admin_Controller
 {	
 	protected $msg1 = '请选择查询条件.';
 	
@@ -36,15 +36,9 @@ class Logo extends CI_Controller
 		$this->load->helper('news');     //自定义news_helper
 		$this->load->helper('kakou');    //自定义kakou_helper
 		
-		$this->load->library('DX_Auth');
-		$this->load->library('form_validation');
 		$this->load->library('Lib_kakou');
-		#$this->load->library('MY_Session');
 
-		#$this->load->model('Mgate');
-		#$this->load->model('Msyst');
 		$this->load->model('Mlogo');
-		$this->load->model('Msms');
 		$this->load->model('Mbasedata');
 		
 		$this->load->config('basedata');
@@ -60,48 +54,56 @@ class Logo extends CI_Controller
 		}
 	}
 	
-	//品牌查询
+    /**
+     * 品牌查询
+     * 
+     * @return void
+     */
 	function lgquery()
 	{
-		$data['page'] = $this->input->post('page') ? intval($this->input->post('page')) : 1;
-		$data['rows'] = $this->input->post('rows') ? intval($this->input->post('rows')) : 20;
-		$data['sort'] = $this->input->post('sort') ? $this->input->post('sort') : 'i.id';
-	    $data['order'] = $this->input->post('order') ? $this->input->post('order') : 'desc';
+		$data['page']   = $this->input->post('page') ? intval($this->input->post('page')) : 1;
+		$data['rows']   = $this->input->post('rows') ? intval($this->input->post('rows')) : 20;
+		$data['sort']   = $this->input->post('sort') ? $this->input->post('sort') : 'i.id';
+	    $data['order']  = $this->input->post('order') ? $this->input->post('order') : 'desc';
 		$data['offset'] = ($data['page'] - 1) * $data['rows'];
 
-		$data['number'] = $this->input->post('number') ? $this->input->post('number') : '？'; #‘？’是中文字体
+		$data['number'] = $this->input->post('number') ? $this->input->post('number') : '？'; // '？'是中文字体
 		$data['carnum'] = $this->input->post('carnum') ? $this->input->post('carnum') : '';
-		$data['place'] = $this->input->post('place') ? $this->input->post('place') : 'all';
-		$data['dire'] = $this->input->post('dire') ? $this->input->post('dire') : 'all';
-		$data['hpys'] = $this->input->post('hpys') ? $this->input->post('hpys') : 'all';
-		$data['ppdm'] = $this->input->post('ppdm') ? $this->input->post('ppdm') : 'all';
-		$data['ppdm2'] = $this->input->post('ppdm2') ? $this->input->post('ppdm2') : 'all';
-		$data['cllx'] = $this->input->post('cllx') ? $this->input->post('cllx') : 'all';
-		$data['csys'] = $this->input->post('csys') ? $this->input->post('csys') : 'all';
-		$data['hpys'] = $this->input->post('hpys') ? $this->input->post('hpys') : 'all';
-		$data['st'] = $this->input->post('st') ? $this->input->post('st') : mdate("%Y-%m-%d %H:%i:%s",strtotime("-2 hours"));;
-		$data['et'] = $this->input->post('et') ? $this->input->post('et') : mdate("%Y-%m-%d %H:%i:%s");
-		$data['lane'] = 'all';
-		$data['platename'] = h_create_platename($data['number'],$data['carnum']);
+		$data['place']  = $this->input->post('place') ? $this->input->post('place') : 'all';
+		$data['dire']   = $this->input->post('dire') ? $this->input->post('dire') : 'all';
+		$data['hpys']   = $this->input->post('hpys') ? $this->input->post('hpys') : 'all';
+		$data['ppdm']   = $this->input->post('ppdm') ? $this->input->post('ppdm') : 'all';
+		$data['ppdm2']  = $this->input->post('ppdm2') ? $this->input->post('ppdm2') : 'all';
+		$data['cllx']   = $this->input->post('cllx') ? $this->input->post('cllx') : 'all';
+		$data['csys']   = $this->input->post('csys') ? $this->input->post('csys') : 'all';
+		$data['hpys']   = $this->input->post('hpys') ? $this->input->post('hpys') : 'all';
+		$data['st']     = $this->input->post('st') ? $this->input->post('st') : mdate("%Y-%m-%d %H:%i:%s", strtotime("-2 hours"));;
+		$data['et']     = $this->input->post('et') ? $this->input->post('et') : mdate("%Y-%m-%d %H:%i:%s");
+		$data['lane']   = 'all';
+		$data['platename'] = h_create_platename($data['number'], $data['carnum']);
 
 		$_SESSION['lgquery'] = $data;
 
-		$data['result'] = $this->Mlogo->get_carinfo3($data['offset'],$data['rows'],$data['sort'],$data['order'],$data)->result_array();
-		$data['total'] = $this->Mlogo->get_carinfo3(0,0,$data['sort'],$data['order'],$data)->row()->sum;
+		$data['result'] = $this->Mlogo->get_carinfo3($data['offset'], $data['rows'], $data['sort'], $data['order'], $data)->result_array();
+		$data['total']  = $this->Mlogo->get_carinfo3(0, 0, $data['sort'], $data['order'], $data)->row()->sum;
 
 		$data['title'] = '品牌查询';
 
-		$data['sel_place'] = $this->Mbasedata->get_places()->result_array();
-		$data['sel_dire'] = $this->Mbasedata->get_directions()->result_array();
-		$data['sel_hpys'] = $this->Mbasedata->get_hpys()->result_array();
-		$data['sel_ppdm'] = $this->Mbasedata->get_ppdm()->result_array();
-		$data['sel_cllx'] = $this->Mbasedata->get_cllx()->result_array();
+		$data['sel_place']  = $this->Mbasedata->get_places()->result_array();
+		$data['sel_dire']   = $this->Mbasedata->get_directions()->result_array();
+		$data['sel_hpys']   = $this->Mbasedata->get_hpys()->result_array();
+		$data['sel_ppdm']   = $this->Mbasedata->get_ppdm()->result_array();
+		$data['sel_cllx']   = $this->Mbasedata->get_cllx()->result_array();
 		$data['sel_number'] = $this->config->item('number');
 
 		$this->load->view('logo/lgquery_view', $data);
 	}
 
-	// 短信添加dialog
+    /**
+     * 品牌查询
+     * 
+     * @return void
+     */
 	function lgquery_detail()
 	{
 		$this->load->view('logo/lgquery_detail');
@@ -111,25 +113,25 @@ class Logo extends CI_Controller
 	//比对查询
 	function cmpquery()
 	{
-		$data['page'] = $this->input->post('page') ? intval($this->input->post('page')) : 1;
-		$data['rows'] = $this->input->post('rows') ? intval($this->input->post('rows')) : 20;
-		$data['sort'] = $this->input->post('sort') ? $this->input->post('sort') : 'i.id';
-	    $data['order'] = $this->input->post('order') ? $this->input->post('order') : 'desc';
+		$data['page']   = $this->input->post('page') ? intval($this->input->post('page')) : 1;
+		$data['rows']   = $this->input->post('rows') ? intval($this->input->post('rows')) : 20;
+		$data['sort']   = $this->input->post('sort') ? $this->input->post('sort') : 'i.id';
+	    $data['order']  = $this->input->post('order') ? $this->input->post('order') : 'desc';
 		$data['offset'] = ($data['page'] - 1) * $data['rows'];
 
-		$data['number'] = $this->input->post('number') ? $this->input->post('number') : '？'; #‘？’是中文字体
-		$data['carnum'] = $this->input->post('carnum') ? $this->input->post('carnum') : '';
-		$data['place'] = $this->input->post('place') ? $this->input->post('place') : 'all';
-		$data['dire'] = $this->input->post('dire') ? $this->input->post('dire') : 'all';
-		$data['ppdm'] = $this->input->post('ppdm') ? $this->input->post('ppdm') : 'all';
-		$data['ppdm2'] = $this->input->post('ppdm2') ? $this->input->post('ppdm2') : 'all';
-		$data['confirm'] = $this->input->post(NULL) ? $this->input->post('confirm') : 'all';
+		$data['number']   = $this->input->post('number') ? $this->input->post('number') : '？'; #‘？’是中文字体
+		$data['carnum']   = $this->input->post('carnum') ? $this->input->post('carnum') : '';
+		$data['place']    = $this->input->post('place') ? $this->input->post('place') : 'all';
+		$data['dire']     = $this->input->post('dire') ? $this->input->post('dire') : 'all';
+		$data['ppdm']     = $this->input->post('ppdm') ? $this->input->post('ppdm') : 'all';
+		$data['ppdm2']    = $this->input->post('ppdm2') ? $this->input->post('ppdm2') : 'all';
+		$data['confirm']  = $this->input->post(NULL) ? $this->input->post('confirm') : 'all';
 		$data['clppflag'] = $this->input->post(NULL) ? $this->input->post('clppflag') : 'all';
-		$data['smsflag'] = $this->input->post(NULL) ?  $this->input->post('smsflag') : 'all';
-		$data['st'] = $this->input->post('st') ? $this->input->post('st') : mdate("%Y-%m-%d %H:%i:%s",strtotime("-2 hours"));;
-		$data['et'] = $this->input->post('et') ? $this->input->post('et') : mdate("%Y-%m-%d %H:%i:%s");
-		$data['lane'] = 'all';
-		$data['platename'] = h_create_platename($data['number'],$data['carnum']);
+		$data['smsflag']  = $this->input->post(NULL) ?  $this->input->post('smsflag') : 'all';
+		$data['st']       = $this->input->post('st') ? $this->input->post('st') : mdate("%Y-%m-%d %H:%i:%s",strtotime("-2 hours"));;
+		$data['et']       = $this->input->post('et') ? $this->input->post('et') : mdate("%Y-%m-%d %H:%i:%s");
+		$data['lane']     = 'all';
+		$data['platename']= h_create_platename($data['number'],$data['carnum']);
 
 		$_SESSION['cmpquery'] = $data;
 
@@ -166,131 +168,24 @@ class Logo extends CI_Controller
 
 	}
 	
-	function real_select()
+    /**
+     * 实时信息查询
+     * 
+     * @return void
+     */
+	public function realSelectView()
 	{
-		$data['page'] = $this->input->post('page') ? intval($this->input->post('page')) : 1;
-		$data['rows'] = $this->input->post('rows') ? intval($this->input->post('rows')) : 20;
-		$data['sort'] = $this->input->post('sort') ? $this->input->post('sort') : 'i.id';
-	    $data['order'] = $this->input->post('order') ? $this->input->post('order') : 'desc';
-		$data['offset'] = ($data['page'] - 1) * $data['rows'];
-
-		$data['clppflag'] = $this->input->post(NULL) ? $this->input->post('clppflag') : 'all';
-
-		$query = $this->Mbasedata->get_places();
-		$data['result'] = $query->result_array();
-		$data['total'] = $query->num_rows();
+		$place_array = $_SESSION['role_id'] == 1 ? array() : $_SESSION['role_openkk'];
+		$data['sel_places'] = $this->Mbasedata->getPlaces($place_array)->result_array();
 
 		$data['title'] = '实时信息';
-
-		$data['sel_place'] = $this->Mbasedata->get_places()->result_array();
-		$data['sel_dire'] = $this->Mbasedata->get_directions()->result_array();
-		$data['sel_ppdm'] = $this->Mbasedata->get_ppdm()->result_array();
-		$data['sel_number'] = $this->config->item('number');
 
 		$this->load->view('logo/real_select_view', $data);
 	}
 
-	// 短信设置列表
-	function smsnum()
+	public function select()
 	{
-		$data['page'] = $this->input->post('page') ? intval($this->input->post('page')) : 1;
-		$data['rows'] = $this->input->post('rows') ? intval($this->input->post('rows')) : 20;
-		$data['sort'] = $this->input->post('sort') ? $this->input->post('sort') : 'i.id';
-	    $data['order'] = $this->input->post('order') ? $this->input->post('order') : 'desc';
-		$data['offset'] = ($data['page'] - 1) * $data['rows'];
-
-		$data['result'] = $this->Msms->get_sms($data['offset'],$data['rows'],$data['sort'],$data['order'],$data)->result_array();
-		$data['total'] = $this->Msms->get_sms(0,0,$data['sort'],$data['order'],$data)->row()->sum;
-
-		$data['title'] = '短信编辑';
-
-		$this->load->view('sms/sms_view', $data);
-	}
-
-	// 短信添加dialog
-	function sms_add_view()
-	{
-		$this->load->view('sms/sms_add');
-	}
-
-	// 短信编辑dialog
-	function sms_edit_view()
-	{
-		$id = $this->input->get('id',True);
-		
-		$query = $this->Msms->get_sms_by_id($id);
-
-		$data = $query->row_array();
-
-		$this->load->view('sms/sms_edit', $data);
-	}
-
-
-	// 添加短信
-	function sms_add() 
-	{
-		$data['tel'] = $this->input->post('tel', True);
-		$data['mark'] = $this->input->post('mark', True);
-
-		$data['tel'] = str_replace("，", ",", $data['tel']);
-		
-		$result = array();
-		if ($this->Msms->add_sms($data)) {
-			$result["statusCode"] = "200";
-			$result["message"] = "短信添加完成！";
-            $result["navTabId"] = "smsnum";
-            $result["forwardUrl"] = base_url() . "index.php/logo/smsnum";
-            $result["callbackType"] = "closeCurrent";
-		} else {
-			$result["statusCode"] = "300";
-			$result["message"] = "短信添加失败，请稍后再试！";
-		}
-		
-		echo json_encode($result);
-	}
-	
-	// 编辑短信
-	function sms_edit() 
-	{	
-		$id = $this->input->post('id',True);
-		
-		$data['tel'] = $this->input->post('tel', True);
-		$data['mark'] = $this->input->post('mark', True);
-		
-		$data['tel'] = str_replace("，", ",", $data['tel']);
-		
-		$result = array();
-		if ($this->Msms->edit_sms($id, $data)) {
-			$result["statusCode"] = "200";
-			$result["message"] = "短信编辑成功！";
-            $result["navTabId"] = "smsnum";
-            $result["forwardUrl"] = base_url() . "index.php/logo/smsnum";
-            $result["callbackType"] = "closeCurrent";
-		} else {
-			$result["statusCode"] = "300";
-			$result["message"] = "短信编辑失败，请稍后再试！";
-		}
-		
-		echo json_encode($result);
-	}
-	
-	// 删除短信
-	function sms_del()
-	{
-		$id = $this->input->get('id',True);
-		
-		$result = array();
-		$data['banned'] = 1;
-		if ($this->Msms->edit_sms($id, $data)) {
-			$result['statusCode'] = "200";
-			$result['message'] = "短信删除成功！";
-			$result["navTabId"] = "smsnum";
-		} else {
-			$result['statusCode'] = "300";
-			$result['message'] = "短信删除失败，请稍后再试！"; 
-		}
-
-		echo json_encode($result);
+		var_dump($this->input->post(NULL));
 	}
 
 }
