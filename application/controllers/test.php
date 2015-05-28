b636a1e1468b630387ea13a68625e7d0ac68e689
SmellyCat <smellycat2014@foxmail.com>
2015-05-28 19:52:21 +0800

    Update controllers\test

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -19,248 +19,13 @@ class Test extends CI_Controller
 		$this->load->library('PHPExcel/IOFactory');
 		$this->load->library('Lib_kakou');
 
+		session_start();
+
 	//$this->test_db = $this->load->database('test_db', TRUE);
 	//$this->ora_db = $this->load->database('oracle_db', TRUE);
 	}
 
-	//超时
-	function time_out()
-	{
-		showmessage3('登录超时，请重新登录', 'admin/exit_system');
-	}
 
-    //导出数据到Excel
-    function exportexcel()
-    {
-    	ob_clean();
-/*        $this->load->library('PHPExcel');
-        $this->load->library('PHPExcel/IOFactory');	*/
-        
-/*        $role_id = $this->session->userdata('DX_role_id');
-        $data = $this->_get_openkakou_by_perm($role_id);*/
-        
-      /*   $data['sel_type_alias'] = $this->input->get('type_alias');    //监控点名称
-        
-    	if($role_id == '1')
-		{
-			$da['okkval'] = '';
-		}
-		elseif ($data['sel_type_alias'] == 'all')
-		{
-			$da['okkval'] = implode(",",$data['okkval']);
-		}else 
-		{
-			$da['okkval'] = "'" . $data['sel_type_alias'] . "'";
-		}    
-		
-		$da['lane']      = $this->input->get('lane');           //车道
-		$da['direction'] = $this->input->get('direction');      //方向
-		$da['color']     = $this->input->get('color');          //颜色
-        $da['number']    = $this->input->get('number');         //车牌编号
-        $da['carnum']    = trim($this->input->get('carnum'));   //车牌号码
-        
-        if($this->input->get('spcarnum') != '')
-        {
-        	$spcarnum = explode(",", $this->input->get('spcarnum'));   //特殊车牌号
-            foreach ($spcarnum as $id=>$sp)
-            {
-        	    $da['sp'][$id] = "'" . $sp . "'";
-            }
-            $da['spcarnum'] = implode(",",$da['sp']);
-        }else 
-        {
-            $da['spcarnum'] = '';
-        }
-    
-        $da['starttime'] = $this->input->get('starttime');   //开始时间
-        $da['endtime'] = $this->input->get('endtime');       //结束时间
-
-        $query = $this->Mgate->get_car_by_condition(0, 1000, $da);*/
-        
-        $query = $this->_get_excel_data();
-        //$query = $this->db->query("select * from menu");
-        //$query = mb_convert_encoding("gbk", "UTF-8", $query);
-        
-/*        if(!$query)
-        return false;*/
- 
-        $objPHPExcel = new PHPExcel();
-        $objPHPExcel->getProperties()->setTitle("卡口系统数据")
-                                     ->setDescription("卡口系统生成的车辆查询数据");
- 
-        $objPHPExcel->setActiveSheetIndex(0)
-                    ->setCellValue('A1', iconv('gbk', 'utf-8', '卡口系统Excel数据表'))
-                    ->setCellValue('B2', '中文world!')
-                    ->setCellValue('C1', 'Hello');
-        // Field names in the first row
-        $fields_name = array('号牌号码','号牌颜色','通过时间','监控点名称','方向','车道');
-        $fields = array('HPHM','HPYS','PASSTIME','WZDD','FXBH','CDBH');
-        //$fields = $query->list_fields();
-        $col = 0;
-        foreach ($fields_name as $field)
-        {
-            $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($col, 2, iconv('gbk','utf-8',$field));
-            $col++;
-        }
- 
-        // Fetching the table data
-        $row = 3;
-        foreach($query->result() as $data)
-        {
-            $col = 0;
-            foreach ($fields as $field)
-            {
-                $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($col, $row, iconv('gbk','utf-8', $data->$field));
-                $col++;
-            }
-            $row++;
-        }
- 
-        $objPHPExcel->setActiveSheetIndex(0);
- 
-        $objWriter = IOFactory::createWriter($objPHPExcel, 'Excel5');
-        
-        $to_date =  mdate("%Y-%m-%d %h:%i:%s");
- 
-        //发送标题强制用户下载文件
-        header('Content-Type: application/vnd.ms-excel;charset=gbk');
-        header('Content-Disposition: attachment;filename="kakou_excel.xls"');
-        header('Cache-Control: max-age=0');
- 
-        $objWriter->save('php://output');
-    }
-    
-function excel()
-{
-ob_clean();
-
-		//$sys['type_name']  = '卡口名称';
-		//$sys['type_value'] = '';
-/*		    
-		$openkakou_all = $this->Msyst->get_config_info($sys)->result();*/
-$this->_get_excel_data();		
-$this->load->database();
-$query = $this->db->query("select * from menu");
-// $query = mb_convert_encoding("gb2312", "UTF-8", $query);
-if(!$query)
-return false;
- 
- 
-$objPHPExcel = new PHPExcel();
-$objPHPExcel->getProperties()->setTitle("卡口系统数据")
-                             ->setDescription("卡口系统生成的车辆查询数据");
- 
-$objPHPExcel->setActiveSheetIndex(0)
-            ->setCellValue('A1', iconv('gbk', 'utf-8', '卡口Hello'))
-            ->setCellValue('B2', '中文world!')
-            ->setCellValue('C1', 'Hello');
-// Field names in the first row
-$fields = $query->list_fields();
-$col = 0;
-foreach ($fields as $field)
-{
-$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($col, 2, iconv('gbk','utf-8', $field));
-$col++;
-}
- 
-// Fetching the table data
-$row = 3;
-foreach($query->result() as $data)
-{
-$col = 0;
-foreach ($fields as $field)
-{
-$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($col, $row, iconv('gbk','utf-8', $data->$field));
-$col++;
-}
- 
-$row++;
-}
- 
-$objPHPExcel->setActiveSheetIndex(0);
- 
-$objWriter = IOFactory::createWriter($objPHPExcel, 'Excel5');
- 
-//发送标题强制用户下载文件
-header('Content-Type: application/vnd.ms-excel;charset=UTF-8');
-header('Content-Disposition: attachment;filename="PHPExcel_test.xls"');
-header('Cache-Control: max-age=0');
- 
-$objWriter->save('php://output');
-
-//$this->load->view('bmenu/index');
-}
- 
-function test1()
-{
-	//index();
-	$car = array('闽','粤','苏','浙','沪','京','津','冀','晋','蒙','辽','吉','黑','皖','赣','鲁','豫','鄂','湘','桂','琼','川','贵','云','藏','陕','甘','青','宁','新','台','渝','港','澳','军','海','空','沈','北','兰','济','南','成','广');  
-	$letter=array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-	$num=array('0','1','2','3','4','5','6','7','8','9');
-	$extr = array('学', '警');
-	foreach ($letter as $id=>$low)
-	{
-		$upletter[$id] = strtoupper($low);
-	}
-	$all = array_merge($num, $upletter);
-	
-	foreach ($all as $a1)
-	{
-		foreach ($extr as $a2)
-		{
-			//$upword = strtoupper($l);
-			$word = "'". $a1 . $a2 . "'";
-			$this->db->query("insert into lexicon(word) values($word)");
-		}
-		//$upword = strtoupper($c);
-		//$word = "'". $c . "'";
-		//$upword = strtoupper($word);
-		//$this->db->query("insert into lexicon(word) values($word)");
-	}
-	
-	echo $this->db->last_query();
-	
-	$this->load->view('bmenu/index');
-	
-}
-
-function test2()
-{
-	$id = 1586;
-	
-	$this->test_db->select('*');
-	$this->test_db->where('id >=', $id);
-	$this->test_db->where('id <', 10000);
-	$query = $this->test_db->get('carlib');
-	
-	foreach ($query->result() as $row)
-	{
-		$carnum = $row->carnum;
-		
-		for($i=0;$i<(strlen($carnum)-2);$i++)
-		{
-			$section = mb_substr($carnum, $i, 2, 'gbk');
-			
-			$res= $this->test_db->select('*')->where('word',$section)->get('lexicon');
-			
-			if($res->num_rows()>0)
-			{
-				$lex_id = $res->row()->id;
-				
-				$this->test_db->query("insert into lex_car(lex_id, car_id, position) values($lex_id, $row->id, $i)");
-			}
-			else 
-			{
-				$this->test_db->query("insert into miss(miss_word, carlib_id) values('$section', $row->id)");
-			}
-		}
-	}
-	
-	echo $this->test_db->query();
-	
-	$this->load->view('bmenu/index');
-	
-}
 
 //录入车牌号码到carlib
 function test3()
@@ -373,6 +138,31 @@ function _get_excel_data()
 	{
 		$data = array('马刺','牛宿','热火');
 		echo h_arr_to_str($data);
+	}
+
+	function empty_test()
+	{
+		$arr = array();
+		var_dump(empty($arr));
+	}
+
+	function place_test()
+	{
+		$this->load->model('Mbasedata');
+		$arr = array('2','13');
+		$res = $this->Mbasedata->getPlaces($arr)->result_array();
+		var_dump($res);
+	}
+
+	function explode_test()
+	{
+		$str = '1,2,3';
+		var_dump(explode(',', $str));
+	}
+
+	function show_session()
+	{
+		var_dump($_SESSION);
 	}
 
 }
