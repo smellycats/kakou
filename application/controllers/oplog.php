f01ee08758cf2a6a6dee9b783751c47a3610b95a
SmellyCat <smellycat2014@foxmail.com>
2015-05-28 23:00:20 +0800

    Update oplog

--- application/controllers/oplog.php
+++ application/controllers/oplog.php
@@ -28,18 +28,18 @@ class Oplog extends Admin_Controller
 	//操作日志列表
 	function view()
 	{
-		$data['page'] = $this->input->post('page') ? intval($this->input->post('page')) : 1;
-		$data['rows'] = $this->input->post('rows') ? intval($this->input->post('rows')) : 20;
-		$data['sort'] = $this->input->post('sort') ? $this->input->post('sort') : 'czsj';
-	    $data['order'] = $this->input->post('order') ? $this->input->post('order') : 'desc';
+		$data['page']   = $this->input->post('page') ? intval($this->input->post('page')) : 1;
+		$data['rows']   = $this->input->post('rows') ? intval($this->input->post('rows')) : 20;
+		$data['sort']   = $this->input->post('sort') ? $this->input->post('sort') : 'czsj';
+	    $data['order']  = $this->input->post('order') ? $this->input->post('order') : 'desc';
 		$data['offset'] = ($data['page'] - 1) * $data['rows'];
 		
-		$data['st'] = $this->input->post('st') ? $this->input->post('st') : mdate('%Y-%m-%d %H:%i:%s',strtotime('-24 hours'));;
-		$data['et'] = $this->input->post('et') ? $this->input->post('et') : mdate('%Y-%m-%d %H:%i:%s');
+		$data['st']    = $this->input->post('st') ? $this->input->post('st') : mdate('%Y-%m-%d %H:%i:%s',strtotime('-24 hours'));;
+		$data['et']    = $this->input->post('et') ? $this->input->post('et') : mdate('%Y-%m-%d %H:%i:%s');
 	    $data['uname'] = $this->input->post('uname') ? $this->input->post('uname') : '';
 
-		$data['result'] = $this->Muserlog->get_userlogs($data['offset'],$data['rows'],$data['sort'],$data['order'],$data)->result_array();
-		$data['total'] = $this->Muserlog->get_userlogs(0,0,$data['sort'],$data['order'],$data)->row()->sum;
+		$data['result'] = $this->Muserlog->getUserlogs($data['offset'], $data['rows'], $data['sort'], $data['order'], $data)->result_array();
+		$data['total']  = $this->Muserlog->getUserlogs(0, 0, $data['sort'], $data['order'], $data)->row()->sum;
 
 		$data['title'] = '操作日志列表';
 
